80876dabf3b315a8db5824cd7cb3abed7341d9c8
Ali Salman <eng.aliabdullah@gmail.com>
2017-07-23 13:24:20 -0500

    simple JS code with functional programming, taking an array multiply it and output the new array

--- assets/app.js
+++ assets/app.js
@@ -8,7 +8,33 @@ for (var i = 0; i < inputArr.length; i++) {     // Loop through the array elemen
         inputArr[i] * 2     // map an array and do something to it's elements
     );
 };
-// console the old and new array
+// console the input and new array
 console.log(inputArr);
 console.log(outputArr);
+console.log('----------------------')
 
+
+// Using functional programming to re-code the above example
+
+// create a function that take an input arr and another function
+function mapForEach(arr, fn) {
+    // new array to have the result of mapping arr
+    var newArr = [];
+    for (var i = 0; i < arr.length; i++) {
+        newArr.push(
+            fn(arr[i])
+        );
+    };
+    return newArr;
+}
+
+var inputArr1 = [1,2,3,4,5];
+
+var outputArr1 = mapForEach(inputArr1, function(item) {
+    return item * 2;
+});
+
+// console the input and new array
+console.log(inputArr1);
+console.log(outputArr1);
+console.log('----------------------')
