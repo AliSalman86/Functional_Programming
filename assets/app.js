d7bd96703f334a6875748ec39703ae119464104a
Ali Salman <eng.aliabdullah@gmail.com>
2017-07-23 13:54:43 -0500

    adding example 3 with a limit checkr function

--- assets/app.js
+++ assets/app.js
@@ -16,14 +16,21 @@ console.log('----------------------')
 
 // Using functional programming to re-code the above example
 
+// ex1:
 // create a function that take an input arr and another function
 function mapForEach(arr, fn) {
     // new array to have the result of mapping arr
     var newArr = [];
     for (var i = 0; i < arr.length; i++) {
-        newArr.push(
-            fn(arr[i])
-        );
+        var checkr = fn(arr[i]);
+        if (checkr) {
+            newArr.push(checkr);
+        };
+        // if we push the functoin output then in case of ex3, we will have undefined values in the resulted array, check by commenting above
+        // and un-commenting below
+        // newArr.push(
+        //     fn(arr[i])
+        // )
     };
     return newArr;
 }
@@ -38,3 +45,31 @@ var outputArr1 = mapForEach(inputArr1, function(item) {
 console.log(inputArr1);
 console.log(outputArr1);
 console.log('----------------------')
+
+// ex2: another sample of functional programming only with changing the function
+
+var outputArr2 = mapForEach(inputArr1, function(item) {
+    return item > 2;
+});
+
+// output here is boolean values, checking if it false or true every array element is larger than 2.
+console.log(inputArr1);
+console.log(outputArr2);
+console.log('----------------------')
+
+
+// another example --- adding some more functions
+
+var inputArr2 = [12, 43, 67, 109, 3, 143, 90, 7];
+
+var checkLimits = function(upperLimit, lowerLimit, item) {
+    if (item > lowerLimit && item < upperLimit) {
+        return item;
+    };
+};
+
+var outputArr3 = mapForEach(inputArr2, checkLimits.bind(this, 50, 10));
+
+console.log(inputArr2);
+console.log(outputArr3);
+console.log('----------------------');
